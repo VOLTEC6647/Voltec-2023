3ebda58932ae1619cb4541cfd9fb99b26fc0dd68
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-01-28 20:07:37 -0600

    Added multipliers and drive constants

--- src/main/java/com/team6647/Constants.java
+++ src/main/java/com/team6647/Constants.java
@@ -6,7 +6,18 @@ package com.team6647;
 
 import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
 public final class Constants {
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+    public static final int kDriverControllerPort2 = 1;
+
+    public static final double xMultiplier = 1;
+    public static final double yMultiplier = 1;
+  }
+
   public static class ChassisConstants {
     public static final int frontLeftID = 1;
     public static final int frontRightID = 2;
@@ -17,8 +28,31 @@ public final class Constants {
         2);
   }
 
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-    public static final int kDriverControllerPort2 = 1;
+  public static class ArmConstants {
+    public static final int armNeo1ID = 5;
+    public static final int armNeo2ID = 6;
+  }
+
+  public static class DriveConstants {
+    public static final double ksVolts = 0;
+    public static final double kvVoltSecondsPerMeter = 0;
+    public static final double kaVoltSecondsSquaredPerMeter = 0;
+    public static final double kpDriveVelocity = 0;
+
+    public static final double kTrackWidthMeters = Units.inchesToMeters(19);
+    public static final DifferentialDriveKinematics kDrivekinematics = new DifferentialDriveKinematics(
+        kTrackWidthMeters);
+
+    public static final double kramseteB = 2;
+    public static final double kRamseteZeta = 0.7;
+
+    /**
+     * Make sure that the reduction is not in place
+     */
+    public static final double kGearRatio = 7.29;
+
+    public static final double kWheelRadiusInches = 3;
+    public static final double kWheelDiameterMeter = Units.inchesToMeters(kWheelRadiusInches);
+    public static final double kWheelCircumference = kWheelDiameterMeter * Math.PI;
   }
 }
