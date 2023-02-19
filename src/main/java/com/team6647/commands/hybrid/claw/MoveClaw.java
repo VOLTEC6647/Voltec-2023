748fd6a8d786a2972c24a402a8289b1a50068084
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-02-18 20:32:45 -0600

    Added claw folder

--- src/main/java/com/team6647/commands/hybrid/claw/MoveClaw.java
+++ src/main/java/com/team6647/commands/hybrid/claw/MoveClaw.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package com.team6647.commands.hybrid.Arm;
+package com.team6647.commands.hybrid.claw;
 
 import com.team6647.subsystems.ClawSubsytem;
 
