5351f7735413794f20e07c6e366619bde8a39e97
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-02-28 22:28:23 -0600

    Fixed arm limit switch

--- src/main/java/com/team6647/commands/hybrid/Arm/ExtendArm.java
+++ src/main/java/com/team6647/commands/hybrid/Arm/ExtendArm.java
@@ -4,6 +4,8 @@
 
 package com.team6647.commands.hybrid.Arm;
 
+import org.opencv.objdetect.FaceDetectorYN;
+
 import com.team6647.Constants.ArmConstants;
 import com.team6647.subsystems.ArmSubsystem;
 
@@ -22,25 +24,42 @@ public class ExtendArm extends CommandBase {
 
   @Override
   public void initialize() {
+    System.out.println(speed);
+    if (!arm.getLimitState()) {
+      arm.resetExtendPosition();
+      end(true);
+      return;
+    } else if (Math.abs(arm.getExtendPosition()) >= Math.abs(ArmConstants.forwardLimit)) {
+      end(true);
+      return;
+    }
+    /*
+     * if (speed > 0 && !arm.getLimitState()) {
+     * System.out.println("TAAAAA");
+     * arm.extendArm(speed);
+     * } else if (speed < 0 && Math.abs(arm.getExtendPosition()) >=
+     * Math.abs(ArmConstants.forwardLimit)) {
+     * System.out.println("WAAA");
+     * arm.extendArm(speed);
+     * } else if (!arm.getLimitState()) {
+     * arm.resetExtendPosition();
+     * System.out.println("TOUCHED");
+     * end(true);
+     * return;
+     * } else if (Math.abs(arm.getExtendPosition()) >=
+     * Math.abs(ArmConstants.forwardLimit)) {
+     * end(true);
+     * System.out.println("AAAA");
+     * return;
+     * }
+     */
+
+    arm.extendArm(speed);
+
   }
 
   @Override
   public void execute() {
-    if (speed <= 0) {
-      if (!arm.getLimitState()) {
-        arm.resetExtendPosition();
-        arm.extendArm(0);
-        end(true);
-      } else {
-        arm.extendArm(speed);
-      }
-    } else if (speed > 0) {
-      if (Math.abs(arm.getExtendPosition()) >= Math.abs(ArmConstants.forwardLimit)) {
-        end(true);
-      } else {
-        arm.extendArm(speed);
-      }
-    }
   }
 
   @Override
@@ -50,6 +69,12 @@ public class ExtendArm extends CommandBase {
 
   @Override
   public boolean isFinished() {
-    return false;
+    if ((speed > 0 && !arm.getLimitState())
+        || (speed < 0 && Math.abs(arm.getExtendPosition()) >= Math.abs(ArmConstants.forwardLimit))) {
+      System.out.println("FINISSSSAA");
+      return false;
+    }
+    System.out.println("SDBIUAFBD");
+    return !arm.getLimitState() || (Math.abs(arm.getExtendPosition()) >= Math.abs(ArmConstants.forwardLimit));
   }
 }
