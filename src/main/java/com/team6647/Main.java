97987b55bb75f11830c89056ffefa099f7696d3a
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-02-28 22:35:50 -0600

    Special one

--- src/main/java/com/team6647/Main.java
+++ src/main/java/com/team6647/Main.java
@@ -1,24 +1,21 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/**
+ * Written by Juan Pablo Gutiérrez on Jan 7, 2023 
+ * In honor of HRGD
+ * 
+ * Team 6647
+ * 
+ * "The difference in winning & losing is most often, not quitting"
+ * Walt Disney
+ */
 
 package com.team6647;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
-/**
- * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
- * you are doing, do not modify this file except to change the parameter class to the startRobot
- * call.
- */
 public final class Main {
-  private Main() {}
+  private Main() {
+  }
 
-  /**
-   * Main initialization function. Do not perform any initialization here.
-   *
-   * <p>If you change your main robot class, change the parameter type.
-   */
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
   }
