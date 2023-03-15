9159897c27fd4259b280deadf7d18e2195fd5ebc
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-03-14 22:41:24 -0600

    Absolute encoder

--- src/main/java/com/team6647/robot/Robot.java
+++ src/main/java/com/team6647/robot/Robot.java
@@ -7,6 +7,9 @@ package com.team6647.robot;
 import com.andromedalib.robot.SuperRobot;
 import com.team6647.subsystems.DriveSubsystem;
 
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Robot extends SuperRobot {
 
   private RobotContainer robotContainer;
@@ -30,5 +33,16 @@ public class Robot extends SuperRobot {
   public void teleopInit() {
     super.teleopInit();
     robotContainer.setChassisCommand();
+  }
+
+
+  DutyCycleEncoder absEncoder = new DutyCycleEncoder(4);
+
+  @Override
+  public void teleopPeriodic(){
+    super.teleopPeriodic();
+    double val = ((absEncoder.getAbsolutePosition() * 2048.0 )/ 360) - 148.7;
+    SmartDashboard.putNumber("Position", absEncoder.getDistance());
+    SmartDashboard.putNumber("Position Absolute", absEncoder.getAbsolutePosition());
   }
 }
