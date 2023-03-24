46c1305916a80fe564ee9f9fd3dec2aca17adb8e
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-03-23 21:54:39 -0400

    Removed robot changes

--- src/main/java/com/team6647/robot/Robot.java
+++ src/main/java/com/team6647/robot/Robot.java
@@ -7,7 +7,6 @@ package com.team6647.robot;
 import com.andromedalib.robot.SuperRobot;
 import com.team6647.subsystems.DriveSubsystem;
 
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -36,16 +35,5 @@ public class Robot extends SuperRobot {
   public void teleopInit() {
     super.teleopInit();
     robotContainer.setChassisCommand();
-  }
-
-
-  DutyCycleEncoder absEncoder = new DutyCycleEncoder(4);
-
-  @Override
-  public void teleopPeriodic(){
-    super.teleopPeriodic();
-    double val = ((absEncoder.getAbsolutePosition() * 2048.0 )/ 360) - 148.7;
-    SmartDashboard.putNumber("Position", absEncoder.getDistance());
-    SmartDashboard.putNumber("Position Absolute", absEncoder.getAbsolutePosition());
   }
 }
