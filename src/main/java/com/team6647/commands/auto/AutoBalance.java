0e77296cae1d09ad45deafe7de49bc14b8035ace
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-03-26 20:30:15 -0400

    Cleaned up auto balance command

--- src/main/java/com/team6647/commands/auto/AutoBalance.java
+++ src/main/java/com/team6647/commands/auto/AutoBalance.java
@@ -12,24 +12,22 @@ import com.team6647.utils.Constants.DriveConstants;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+/**
+ * Moves the robot until it is in the charge station (balanced)
+ */
 public class AutoBalance extends CommandBase {
   ChassisSubsystem chassis;
   DriveSubsystem drive;
   VisionSubsystem vision;
 
-  double drivePower = 0;
-  double currentAngle = 0;
-  double error = 0;
+  double drivePower, currentAngle, error = 0;
+  
   public AutoBalance(ChassisSubsystem chassis, DriveSubsystem drive) {
     this.chassis = chassis;
     this.drive = drive;
     this.vision = VisionSubsystem.getInstance("Photon");
 
     addRequirements(drive, chassis);
-  }
-
-  @Override
-  public void initialize() {
   }
 
   @Override
@@ -44,7 +42,7 @@ public class AutoBalance extends CommandBase {
     SmartDashboard.putNumber("Drivepower 2: " , drivePower);
 
     if(drivePower < 0){
-      drivePower *= 0.8; //1.35
+      drivePower *= 0.8;
     }
 
     if(Math.abs(drivePower) > 0.35){
@@ -52,10 +50,6 @@ public class AutoBalance extends CommandBase {
     }
 
     chassis.tankDrive(drivePower, drivePower);
-    SmartDashboard.putNumber("Angle: ",currentAngle);
-    SmartDashboard.putNumber("Error: " , error);
-    SmartDashboard.putNumber("Drivepower: " , drivePower);
-
   }
 
   @Override
