1a00409efb0b4c78d3c3797ab34e6a85b9d35983
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-04-09 10:03:22 -0600

    Removed photonvision

--- src/main/java/com/team6647/subsystems/VisionSubsystem.java
+++ src/main/java/com/team6647/subsystems/VisionSubsystem.java
@@ -4,59 +4,22 @@
 
 package com.team6647.subsystems;
 
-import java.util.Optional;
-
-import org.photonvision.EstimatedRobotPose;
-import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonUtils;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-import org.photonvision.targeting.PhotonTrackedTarget;
-
 import com.andromedalib.vision.LimelightCamera;
-import com.team6647.utils.Constants.DriveConstants;
 import com.team6647.utils.Constants.VisionConstants;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionSubsystem extends SubsystemBase {
   private static VisionSubsystem instance;
   private static ChassisSubsystem chassis;
 
-  private AprilTagFieldLayout field;
-  private PhotonCamera photonCamera;
-  private PhotonPoseEstimator photonPoseEstimator;
-
-  private PIDController forwardController = new PIDController(DriveConstants.kpDriveVelocity, 0, 0);
-  private PIDController turnController = new PIDController(DriveConstants.kpDriveVelocity, 0, 0);
-
   private LimelightCamera limelightCamera;
 
-  private boolean aimingPhoton;
   private boolean aimingLimelight;
 
-  private VisionSubsystem(String cameraName) {
-    photonCamera = new PhotonCamera(cameraName);
-    photonCamera.setPipelineIndex(VisionConstants.aprilPhotonPipe);
-
+  private VisionSubsystem() {
     limelightCamera = new LimelightCamera();
     limelightCamera.setPipeline(VisionConstants.retroLimePipe);
-
-    try {
-      field = new AprilTagFieldLayout(AprilTagFields.k2023ChargedUp.m_resourceFile);
-      // TODO SET THE ROBOT TO POSE
-      photonPoseEstimator = new PhotonPoseEstimator(field, PoseStrategy.CLOSEST_TO_CAMERA_HEIGHT, photonCamera, null);
-    } catch (Exception e) {
-      DriverStation.reportWarning("Cannot find field layout", true);
-      photonPoseEstimator = null;
-    }
 
     chassis = ChassisSubsystem.getInstance();
 
@@ -69,62 +32,18 @@ public class VisionSubsystem extends SubsystemBase {
    * 
    * @return {@link VisionSubsystem} global instance
    */
-  public static VisionSubsystem getInstance(String name) {
+  public static VisionSubsystem getInstance() {
     if (instance == null) {
-      instance = new VisionSubsystem(name);
+      instance = new VisionSubsystem();
     }
     return instance;
   }
 
   @Override
   public void periodic() {
-    if (aimingPhoton) {
-      calculatePhoton();
-    }
-
     if (aimingLimelight) {
       calculateLime();
     }
-  }
-
-  /**
-   * Calculates the position to the AprilTags target and applies it to chassis
-   */
-  public void calculatePhoton() {
-    var result = photonCamera.getLatestResult();
-
-    if (!result.hasTargets())
-      return;
-
-    PhotonTrackedTarget target = result.getBestTarget();
-
-    double range = PhotonUtils.calculateDistanceToTargetMeters(VisionConstants.cameraHeight,
-        VisionConstants.targetHeight, VisionConstants.cameraPitch, Units.degreesToRadians(target.getPitch()));
-
-    double forwardSpeed = -forwardController.calculate(range, VisionConstants.goalRange);
-    double turnSpeed = -turnController.calculate(result.getBestTarget().getPitch(), 0);
-
-    double leftSpeed = forwardSpeed + turnSpeed;
-    double rightSpeed = forwardSpeed - turnSpeed;
-
-    chassis.tankDrive(leftSpeed * 0.5, rightSpeed * 0.5);
-    chassis.getDrive().feed();
-  }
-
-  /**
-   * Get the Estimated Global Pose from the {@link PhotonPoseEstimator}. Used to
-   * update the {@link DifferentialDrivePoseEstimator}
-   * 
-   * @param prevEstimatedPosition
-   * @return
-   */
-  public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedPosition) {
-    if (photonPoseEstimator == null) {
-      return Optional.empty();
-    }
-
-    photonPoseEstimator.setReferencePose(prevEstimatedPosition);
-    return photonPoseEstimator.update();
   }
 
   /**
@@ -149,8 +68,6 @@ public class VisionSubsystem extends SubsystemBase {
 
     double leftSpeed = distanceAdjust + steeringAdjust;
     double rightSpeed = distanceAdjust - steeringAdjust;
-    SmartDashboard.putNumber("Lime left speed",leftSpeed);
-    SmartDashboard.putNumber("Lime right speed",rightSpeed);
 
     chassis.getDrive().feed();
     chassis.tankDrive((leftSpeed) * 0.5, (rightSpeed) * 0.5);
@@ -161,22 +78,6 @@ public class VisionSubsystem extends SubsystemBase {
    */
   public void setLimeLEDMode(int mode) {
     limelightCamera.setLedMode(mode);
-  }
-
-  /**
-   * Toggles the aim for PhotonCamera
-   */
-  public void togglePhotonAim() {
-    aimingPhoton = !aimingPhoton;
-  }
-
-  /**
-   * Gets the status of the PhotonAim
-   * 
-   * @return PhotonAim status
-   */
-  public boolean getPhotonAim() {
-    return aimingPhoton;
   }
 
   /**
@@ -213,21 +114,7 @@ public class VisionSubsystem extends SubsystemBase {
     return (int) limelightCamera.getPipeline();
   }
 
-  /**
-   * Sets the {@link PhotonCamera} pipeline
-   * 
-   * @param pipeLine Pipeline to be used
-   */
-  public void setPhotonPipe(int pipeLine) {
-    photonCamera.setPipelineIndex(pipeLine);
-  }
-
-  /**
-   * Get current PhotonCamera Pipe
-   * 
-   * @return Current PhotonCamera pipeline
-   */
-  public int getPhotonPipe() {
-    return photonCamera.getPipelineIndex();
+  public LimelightCamera getCamera(){
+    return limelightCamera;
   }
 }
