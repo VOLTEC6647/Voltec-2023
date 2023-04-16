de7a002ed80b11807e776556943c8ae486092a9e
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-04-15 20:19:38 -0600

    Removed command

--- src/main/java/com/team6647/commands/hybrid/claw/MoveClaw.java
+++ src/main/java/com/team6647/commands/hybrid/claw/MoveClaw.java
@@ -1,55 +1,30 @@
-/**
- * Written by Juan Pablo Gutiérrez
- */
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 package com.team6647.commands.hybrid.claw;
 
 import com.team6647.subsystems.ClawSubsytem;
-import com.team6647.utils.Constants.ClawConstants;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-/**
- * Moves the claw with a desired mult
- */
 public class MoveClaw extends CommandBase {
   ClawSubsytem claw;
-  double speed;
-  double mult;
-  boolean both;
 
-  public MoveClaw(ClawSubsytem claw, double mult, boolean both) {
+  public MoveClaw(ClawSubsytem claw) {
     this.claw = claw;
-    this.mult = mult;
-    this.both = both;
 
     addRequirements(claw);
   }
 
   @Override
-  public void initialize() {
-    if (claw.getSolenoidState() == Value.kForward) {
-      speed = ClawConstants.clawSpeedCubes * mult;
-    } else {
-      speed = ClawConstants.clawSpeedCones * mult;
-    }
-
-    if (both) {
-      claw.setBothVelocity(speed);
-    } else {
-      claw.setVelocity(speed);
-    }
-  }
+  public void initialize() {}
 
   @Override
-  public void execute() {
-  }
+  public void execute() {}
 
   @Override
-  public void end(boolean interrupted) {
-    claw.setBothVelocity(0);
-  }
+  public void end(boolean interrupted) {}
 
   @Override
   public boolean isFinished() {
