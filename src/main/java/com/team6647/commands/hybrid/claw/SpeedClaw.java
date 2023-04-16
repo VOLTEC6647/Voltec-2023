ab7841c22fc896f510731e2f8486cde012df8608
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-04-15 20:18:01 -0600

    Renamed command

---
+++ src/main/java/com/team6647/commands/hybrid/claw/SpeedClaw.java
@@ -0,0 +1,58 @@
+/**
+ * Written by Juan Pablo Gutiérrez
+ */
+
+package com.team6647.commands.hybrid.claw;
+
+import com.team6647.subsystems.ClawSubsytem;
+import com.team6647.utils.Constants.ClawConstants;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * Moves the claw with a desired mult
+ */
+public class SpeedClaw extends CommandBase {
+  ClawSubsytem claw;
+  double speed;
+  double mult;
+  boolean both;
+
+  public SpeedClaw(ClawSubsytem claw, double mult, boolean both) {
+    this.claw = claw;
+    this.mult = mult;
+    this.both = both;
+
+    addRequirements(claw);
+  }
+
+  @Override
+  public void initialize() {
+    if (claw.getSolenoidState() == Value.kForward) {
+      speed = ClawConstants.clawSpeedCubes * mult;
+    } else {
+      speed = ClawConstants.clawSpeedCones * mult;
+    }
+
+    if (both) {
+      claw.setBothVelocity(speed);
+    } else {
+      claw.setVelocity(speed);
+    }
+  }
+
+  @Override
+  public void execute() {
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    claw.setBothVelocity(0);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
