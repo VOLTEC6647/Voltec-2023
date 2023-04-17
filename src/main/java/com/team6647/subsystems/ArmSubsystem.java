879753f351451646d993a703f0c7628390f70a06
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-04-16 20:02:35 -0600

    Moved extend methods to subsystem

--- src/main/java/com/team6647/subsystems/ArmSubsystem.java
+++ src/main/java/com/team6647/subsystems/ArmSubsystem.java
@@ -6,11 +6,12 @@ package com.team6647.subsystems;
 import com.andromedalib.math.Functions;
 import com.andromedalib.motorControllers.SuperSparkMax;
 import com.andromedalib.motorControllers.IdleManager.GlobalIdleMode;
+import com.andromedalib.sensors.RevAbsoluteEncoder;
 import com.team6647.utils.Constants.ArmConstants;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
@@ -19,13 +20,11 @@ public class ArmSubsystem extends SubsystemBase {
   private static SuperSparkMax pivotSpark1 = new SuperSparkMax(ArmConstants.armNeo1ID, GlobalIdleMode.brake, false, 50);
   private static SuperSparkMax pivotSpark2 = new SuperSparkMax(ArmConstants.armNeo2ID, GlobalIdleMode.brake, true, 50);
 
-  private static SuperSparkMax extendingSpark = new SuperSparkMax(ArmConstants.extendNeoID, GlobalIdleMode.brake, true,
-      50);
-
-  private static DigitalInput limitSwitch = new DigitalInput(3);
 
   private static ProfiledPIDController profiledController = new ProfiledPIDController(ArmConstants.pivotkP, 0, 0,
-      new TrapezoidProfile.Constraints(40, 30));
+      new TrapezoidProfile.Constraints(60, 50));
+
+  private static RevAbsoluteEncoder armEncoder = new RevAbsoluteEncoder(7);
 
   private double pidOutput;
   private double feedOutput;
@@ -34,9 +33,10 @@ public class ArmSubsystem extends SubsystemBase {
   private double totalOutput;
 
   public ArmSubsystem() {
+
     pivotSpark2.follow(pivotSpark1, true);
 
-    this.setPoint = -144.2;
+    this.setPoint = -14; //-149
 
     resetPID();
   }
@@ -56,6 +56,9 @@ public class ArmSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     calculateArm();
+
+    SmartDashboard.putNumber("Arm encoder", armEncoder.getAbsolutePosition() -
+        180);
   }
 
   /**
@@ -77,9 +80,9 @@ public class ArmSubsystem extends SubsystemBase {
         feedForwardValue -= (actualPoint - 90) / 112 * 12;
       }
     } else {
-      feedForwardValue = (actualPoint / -144) * 0.31 * 12;
+      feedForwardValue = (actualPoint / -156) * 0.31 * 12;
       if (actualPoint < -90) {
-        feedForwardValue -= (actualPoint + 90) / -144 * 12;
+        feedForwardValue -= (actualPoint + 90) / -156 * 12;
       }
     }
 
@@ -99,10 +102,13 @@ public class ArmSubsystem extends SubsystemBase {
    * 
    * @return Encoder measurement
    */
-  public double getArmPosition() {
-    return (pivotSpark1.getPosition() / ((double) 188 / 360)) -144.2;// - 144.52;// - 148.7;
+  public static double getArmPosition() {
+    return (pivotSpark1.getPosition() / ((double) 188 / 360));// - 154.2;// - 144.52;// - 148.7;
   }
 
+  /**
+   * Sets arm Position to zero
+   */
   public void resetArmPositiom() {
     pivotSpark1.resetEncoder();
   }
@@ -113,8 +119,8 @@ public class ArmSubsystem extends SubsystemBase {
    * @param change Setpoint change
    */
   public void changeSetpoint(double change) {
-    if (change < -144 || change > 112) // TUNE
-      change = Functions.clamp(change, -144, 112);
+    if (change < -156 || change > 112) // TUNE
+      change = Functions.clamp(change, -156, 112);
 
     this.setPoint = change;
   }
@@ -130,49 +136,6 @@ public class ArmSubsystem extends SubsystemBase {
 
   public void resetPID() {
     profiledController.reset(getArmPosition(), 0);
-  }
-
-  /**
-   * Extends the arm at the desired speed
-   * 
-   * @param speed Arm speed
-   */
-  public void extendArm(double speed) {
-    extendingSpark.set(speed);
-  }
-
-  /**
-   * Gets the state of the limit switch
-   * 
-   * @return Current limit switch state
-   */
-  public boolean getLimitState() {
-    return limitSwitch.get();
-  }
-
-  /**
-   * Gets the current position of the extending spark
-   * 
-   * @return Current position
-   */
-  public double getExtendPosition() {
-    return extendingSpark.getPosition();
-  }
-
-  /**
-   * Sets the extending position to 0
-   */
-  public void resetExtendPosition() {
-    extendingSpark.resetEncoder();
-  }
-
-  /**
-   * Gets the voltage applied to Pivot1 Voltage
-   * 
-   * @return Voltage applied
-   */
-  public double getPivot1Voltage() {
-    return pivotSpark1.getBusVoltage();
   }
 
   /**
@@ -211,10 +174,6 @@ public class ArmSubsystem extends SubsystemBase {
     return totalOutput;
   }
 
-  public double getVelociy() {
-    return pivotSpark1.getVelocity();
-  }
-
   /**
    * Gets the temperature of Pivot1
    * 
@@ -231,5 +190,13 @@ public class ArmSubsystem extends SubsystemBase {
    */
   public double getPivot2Temp() {
     return pivotSpark2.getMotorTemperature();
+  }
+
+  /**
+   * Sets arm position to that of the absolute encoder
+   */
+  public static void startArm(){
+    double pos = (47.0 * (armEncoder.getAbsolutePosition() -180)) / 90.0;
+    pivotSpark1.setPosition(pos);
   }
 }
