817d52368e77a14fef54dc7a9c03ee353d6cd248
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-04-16 20:00:25 -0600

    Fixed telemetry

--- src/main/java/com/team6647/utils/shuffleboard/tabs/DebugTab.java
+++ src/main/java/com/team6647/utils/shuffleboard/tabs/DebugTab.java
@@ -10,7 +10,9 @@ import com.team6647.subsystems.ArmSubsystem;
 import com.team6647.subsystems.ChassisSubsystem;
 import com.team6647.subsystems.ClawSubsytem;
 import com.team6647.subsystems.DriveSubsystem;
+import com.team6647.subsystems.TelescopicArm;
 import com.team6647.subsystems.VisionSubsystem;
+import com.team6647.subsystems.WristSubsystem;
 
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -18,8 +20,10 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 public class DebugTab extends ShuffleboardTabBase {
 
     ArmSubsystem arm = ArmSubsystem.getInstance();
+    TelescopicArm teleArm = TelescopicArm.getInstance();
     ChassisSubsystem chassis = ChassisSubsystem.getInstance();
     ClawSubsytem claw = ClawSubsytem.getInstance();
+    WristSubsystem wrist = WristSubsystem.getInstance();
     DriveSubsystem drive = DriveSubsystem.getInstance();
     VisionSubsystem vision = VisionSubsystem.getInstance();
     RobotContainer container = RobotContainer.getInstance();
@@ -41,14 +45,15 @@ public class DebugTab extends ShuffleboardTabBase {
 
     GenericEntry chassisAngle;
 
-    GenericEntry clawTopLimit;
-    GenericEntry clawBottomLimit;
+    GenericEntry wristTopLimit;
+    GenericEntry wristBottomLimit;
 
-    GenericEntry clawPosition;
+    GenericEntry wristPosition;
+    GenericEntry wristSetpoint;
 
     public DebugTab(ShuffleboardTab tab) {
 
-        this.extendingPosition = tab.add("Extend postion", arm.getExtendPosition()).withPosition(5, 1).getEntry();
+        this.extendingPosition = tab.add("Extend postion", teleArm.getExtendPosition()).withPosition(5, 1).getEntry();
 
         this.armPositionDoublePID = tab.add("Arm Position", ArmSubsystem.getArmPosition()).withPosition(3, 1)
                 .getEntry();
@@ -57,23 +62,24 @@ public class DebugTab extends ShuffleboardTabBase {
         this.feedOutput = tab.add("Feed output", arm.getFeedOutput()).withPosition(4, 2).getEntry();
         this.totalOutput = tab.add("Total Output", arm.getTotal()).withPosition(3, 2).getEntry();
 
-        this.armLimit = tab.add("Limit switch", arm.getLimitState()).withPosition(3, 3).getEntry();
+        this.armLimit = tab.add("Limit switch", teleArm.getLimitState()).withPosition(3, 3).getEntry();
 
         this.limeAim = tab.add("Limelight aim", vision.getLimelightAim()).withPosition(4, 3).getEntry();
 
         this.pivot1Temp = tab.add("Pivot1 Temp", arm.getPivot1Temp()).withPosition(6, 3).getEntry();
         this.pivot2Temp = tab.add("Pivot2 Temp", arm.getPivot2Temp()).withPosition(7, 3).getEntry();
-        this.chassisAngle = tab.add("Chassis Angle", drive.getNavxRoll()).withPosition(6, 2).getEntry();
+        this.chassisAngle = tab.add("Chassis Angle", drive.getNavxRoll()).withPosition(5, 3).getEntry();
 
-        this.clawTopLimit = tab.add("Claw Top Limit", claw.topLimit()).withPosition(8, 1).getEntry();
-        this.clawBottomLimit = tab.add("Claw Bottom Limit", claw.bottomLimit()).withPosition(9, 1).getEntry();
-        this.clawPosition = tab.add("Claw Position", claw.getWristPosition()).withPosition(9, 2).getEntry();
+        this.wristTopLimit = tab.add("Claw Top Limit", wrist.topLimit()).withPosition(6, 1).getEntry();
+        this.wristBottomLimit = tab.add("Claw Bottom Limit", wrist.bottomLimit()).withPosition(7, 1).getEntry();
+        this.wristPosition = tab.add("Claw Position", wrist.getWristPosition()).withPosition(6, 2).getEntry();
+        this.wristSetpoint = tab.add("Wrist setpoint", wrist.getSetpoint()).withPosition(7, 2).getEntry();
     }
 
     @Override
     public void updateTelemetry() {
 
-        extendingPosition.setDouble(arm.getExtendPosition());
+        extendingPosition.setDouble(teleArm.getExtendPosition());
 
         armPositionDoublePID.setDouble(ArmSubsystem.getArmPosition());
         desiredPosition.setDouble(arm.getSetpoint());
@@ -81,7 +87,7 @@ public class DebugTab extends ShuffleboardTabBase {
         pidOutput.setDouble(arm.getPidOutput());
         totalOutput.setDouble(arm.getTotal());
 
-        armLimit.setBoolean(arm.getLimitState());
+        armLimit.setBoolean(teleArm.getLimitState());
 
         limeAim.setBoolean(vision.getLimelightAim());
 
@@ -90,9 +96,10 @@ public class DebugTab extends ShuffleboardTabBase {
 
         chassisAngle.setDouble(drive.getNavxRoll());
 
-        clawTopLimit.setBoolean(claw.topLimit());
-        clawBottomLimit.setBoolean(claw.bottomLimit());
-        clawPosition.setDouble(claw.getWristPosition());
+        wristTopLimit.setBoolean(wrist.topLimit());
+        wristBottomLimit.setBoolean(wrist.bottomLimit());
+        wristPosition.setDouble(wrist.getWristPosition());
+        wristSetpoint.setDouble(wrist.getSetpoint());
 
     }
 }
