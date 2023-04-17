f633950f7eaa0b646d0f93ff9012c9aab0ade020
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-04-16 20:43:33 -0600

    Added limi

--- src/main/java/com/team6647/commands/hybrid/Arm/ExtendArm.java
+++ src/main/java/com/team6647/commands/hybrid/Arm/ExtendArm.java
@@ -41,13 +41,12 @@ public class ExtendArm extends CommandBase {
 
   @Override
   public boolean isFinished() {
-    return false;
-    /* if (speed > 0 && !arm.getLimitState()) {
+    if (speed > 0 && !arm.getLimitState()) {
       return false;
     }
     if (speed < 0 && Math.abs(arm.getExtendPosition()) >= Math.abs(ArmConstants.forwardLimit)) {
       return false;
     }
-    return !arm.getLimitState() || (Math.abs(arm.getExtendPosition()) >= Math.abs(ArmConstants.forwardLimit)); */
+    return !arm.getLimitState() || (Math.abs(arm.getExtendPosition()) >= Math.abs(ArmConstants.forwardLimit));
   }
 }
