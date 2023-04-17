f285d9c70e6644aac295080a842aa204b27b6c8a
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-04-16 20:00:31 -0600

    Fixed telemetry

--- src/main/java/com/team6647/utils/shuffleboard/tabs/DefaultTab.java
+++ src/main/java/com/team6647/utils/shuffleboard/tabs/DefaultTab.java
@@ -10,6 +10,7 @@ import com.team6647.subsystems.ArmSubsystem;
 import com.team6647.subsystems.ChassisSubsystem;
 import com.team6647.subsystems.ClawSubsytem;
 import com.team6647.subsystems.DriveSubsystem;
+import com.team6647.subsystems.TelescopicArm;
 import com.team6647.subsystems.VisionSubsystem;
 
 import edu.wpi.first.networktables.GenericEntry;
@@ -18,6 +19,7 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 public class DefaultTab extends ShuffleboardTabBase {
 
     ArmSubsystem arm = ArmSubsystem.getInstance();
+    TelescopicArm teleArm = TelescopicArm.getInstance();
     ChassisSubsystem chassis = ChassisSubsystem.getInstance();
     ClawSubsytem claw = ClawSubsytem.getInstance();
     DriveSubsystem drive = DriveSubsystem.getInstance();
@@ -38,7 +40,7 @@ public class DefaultTab extends ShuffleboardTabBase {
     public DefaultTab(ShuffleboardTab tab) {
         this.finishedAuto = tab.add("Finished autp", container.getAutonomousCommand().isFinished()).withPosition(9, 3).getEntry();
 
-        this.armLimit = tab.add("Limit switch", arm.getLimitState()).withPosition(3, 3).getEntry();
+        this.armLimit = tab.add("Limit switch", teleArm.getLimitState()).withPosition(3, 3).getEntry();
 
         this.limeAim = tab.add("Limelight aim", vision.getLimelightAim()).withPosition(4, 3).getEntry();
     }
@@ -49,6 +51,6 @@ public class DefaultTab extends ShuffleboardTabBase {
 
         limeAim.setBoolean(vision.getLimelightAim());
 
-        armLimit.setBoolean(arm.getLimitState());
+        armLimit.setBoolean(teleArm.getLimitState());
     }
 }
